16d345c5c206a29ec2ab5a28304dbda431121446
Matthew Willse <mwillse@gmail.com>
2012-11-20 15:40:18 -0500

    adding jamie's ex files.

---
+++ api/getThings.php
@@ -0,0 +1,27 @@
+<?php
+	$mongo = new Mongo();
+	$db = $mongo->awesomest;
+
+	$totalThings = $db->things->count();
+// db.yourCollection.find().limit(-1).skip(yourRandomNumber).next(). 
+
+	$thing1 = $db->things->find()->limit(1)->skip(rand(0,$totalThings-1))->getNext();
+	$thing2 = $db->things->find()->limit(1)->skip(rand(0,$totalThings-1))->getNext();
+
+
+
+	$thing1['id'] = $thing1['_id']->__toString();
+	$thing2['id'] = $thing2['_id']->__toString();
+
+	unset($thing1['_id']);
+	unset($thing2['_id']);
+
+	$output = array();
+	array_push($output,$thing1);
+	array_push($output,$thing2);
+
+
+	header('Content-type:application/json');
+	header('Access-Control-Allow-Origin: *'); 
+	echo json_encode($output);
+?>
