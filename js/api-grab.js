531aab25294a5e0e2eeb955ae14eeb76d82c64fe
Anthony Terruso <aterruso@gmail.com>
2012-12-07 09:05:24 -0500

    Added api-grab.js

---
+++ js/api-grab.js
@@ -0,0 +1,40 @@
+
+// array = new array instaArray[wtf, lol, hipster, ghetto, fail]; 
+// for (i = 0; i < $instagram.length; i++) { }
+
+var currenttag=["hipster", "wtf", "lol"];
+
+
+//,"wtf","lol","fail"];
+
+	for (i = 0; i < currenttag.length; i++){
+			
+			console.log(currenttag[i]);
+			var url="https://api.instagram.com/v1/tags/"+ currenttag[i] +"/media/recent?count=4&access_token=10239.2fa8a45.08f28ab239bf42a29a70321b2bad4ef0&callback=?";
+			grabInsta();
+			
+			}
+
+
+function grabInsta(){
+
+$.getJSON( url, function(insta){
+
+	console.log(insta);
+
+	
+	$.each(insta.data, function(key,item){
+			// $.each(item, function(subKey, subitem){
+			// var value = (typeof subitem =='string')? subitem : subitem.length;
+
+			time = item.created_time;
+			url = item.images.standard_resolution.url;
+
+			$('body').append('<div><img src="'+ url +'" /></div>');
+
+				// })
+			});
+	});
+
+};
+	
